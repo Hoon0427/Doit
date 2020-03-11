a50cc2336901d278a597fb70d062976aa7842716
Hoon0427 <ikaros977@gmail.com>
2020-03-11 12:41:32 +0900

    twitter_continue

---
+++ Doit_twitter.R
@@ -0,0 +1,20 @@
+#데이터 로드
+twitter <- read.csv("twitter.csv",
+                    header = T,
+                    stringsAsFactors = F,
+                    fileEncoding = "UTF-8")
+#install.packages("reshape")
+library(reshape)
+
+#변수명 수정
+twitter <- rename(twitter,
+                  no = 번호,
+                  id = 계정이름,
+                  date = 작성일,
+                  tw = 내용)
+
+install.packages("stringr")
+library(stringr)
+#특수문자 제거
+twitter$tw <- str_replace_all(twitter$tw, "\\W", " ")
+head(twitter$tw)
